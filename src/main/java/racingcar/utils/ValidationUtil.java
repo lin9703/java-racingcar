c7a61aea6c79162e8bb483bde0cbb39669473c2d
김효린 <hyorin97@naver.com>
2020-08-24 01:58:43 +0900

    docs: README.md에 기능 update

--- src/main/java/racingcar/utils/ValidationUtil.java
+++ src/main/java/racingcar/utils/ValidationUtil.java
@@ -9,4 +9,13 @@ public class ValidationUtil {
             throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
         }
     }
+
+    public void validateNullInputName(String[] carNameList) {
+        if (Arrays.stream(carNameList)
+                .anyMatch(name -> name.length() < 1)) {
+            throw new IllegalArgumentException("자동차 이름은 0글자일 수 없습니다.");
+        }
+    }
+
+
 }
